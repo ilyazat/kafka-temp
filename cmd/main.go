0f71e887588167f5983090ec968c393394e5ac65
arilyazov <arilyazov@sbercloud.ru>
2023-05-05 16:01:55 +0300

    feat/add consume and produce funcs

--- cmd/main.go
+++ cmd/main.go
@@ -2,53 +2,90 @@ package main
 
 import (
 	"context"
-	"errors"
+	"encoding/json"
 	"fmt"
 	"github.com/segmentio/kafka-go"
 	"log"
-	"time"
 )
 
 const (
-	TopicName     = "test-topic"
-	BrokerAddress = "localhost:9092"
-	NumPartitions = 0
+	TopicName = "test_topic"
 )
 
-var ExampleProductions = [3]string{
-	"A spectre is haunting Europe — the spectre of communism.",
-	"All the powers of old Europe have entered into a holy alliance to exorcise this spectre:",
-	"Pope and Tsar, Metternich and Guizot, French Radicals and German police-spies.",
+type KafkaMessage struct {
+	Author      string `json:"author"`
+	Commit      string `json:"commit"`
+	Description string `json:"description"`
+	ProjectKey  string `json:"projectKey"`
 }
 
 func main() {
-	if err := run(); err != nil && !errors.Is(err, context.Canceled) {
-		log.Println(err)
-		log.Fatal("stopping")
+	brokers := []string{"localhost:9092"}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	msgCh := make(chan KafkaMessage, 1)
+	go func() {
+		err := consumeMessage(ctx, brokers, TopicName, msgCh)
+		if err != nil {
+			cancel()
+		}
+	}()
+	go func() {
+		for {
+			select {
+			case msg := <-msgCh:
+				// send to outer API using goroutines
+				fmt.Println(msg)
+			default:
+			}
+		}
+	}()
+}
+
+func sendMsgToJira() {
+
+}
+
+func consumeMessage(ctx context.Context, brokers []string, topic string, msgCh chan KafkaMessage) error {
+	reader := kafka.NewReader(kafka.ReaderConfig{
+		Brokers: brokers,
+		Topic:   topic,
+	})
+
+	for {
+		select {
+		case <-ctx.Done():
+			err := reader.Close()
+			if err != nil {
+				return err
+			}
+			return nil
+		default:
+			msg, err := reader.FetchMessage(ctx)
+			if err != nil {
+				return err
+			}
+			kafkaMsg, err := parseKafkaMsg(&msg)
+			if err != nil {
+				return err
+			}
+			err = reader.CommitMessages(ctx, msg)
+			if err != nil {
+				return err
+			}
+			msgCh <- kafkaMsg
+			return nil
+		}
 	}
 }
 
-func run() error {
-	conn, err := kafka.DialLeader(context.Background(), "tcp", BrokerAddress, TopicName, NumPartitions)
+func parseKafkaMsg(msg *kafka.Message) (KafkaMessage, error) {
+	var kafkaMsg KafkaMessage
+	err := json.Unmarshal(msg.Value, &kafkaMsg)
 	if err != nil {
-		return err
+		log.Printf("Error unmarshalling message: %v", err)
+	} else {
+		log.Printf("Received message: %+v", kafkaMsg)
 	}
-	defer func(c *kafka.Conn) {
-		err := c.Close()
-		if err != nil {
-			log.Fatal("failed to close writer:", err)
-		}
-	}(conn)
-	err = conn.SetWriteDeadline(time.Now().Add(10 * time.Second))
-	if err != nil {
-		return err
-	}
-	for _, str := range ExampleProductions {
-		_, err := conn.WriteMessages(
-			kafka.Message{Value: []byte(str)})
-		if err != nil {
-			return fmt.Errorf("failed to write a message: %v", err)
-		}
-	}
-	return nil
+	return kafkaMsg, err
 }
